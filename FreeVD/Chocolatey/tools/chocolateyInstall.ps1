4dfdff4a85052412e813c08d28a7c9ce5a06c837
Marcus Lum <marcusl@gmail.com>
2017-10-21 20:31:06 -0500

    add choco files

---
+++ FreeVD/Chocolatey/tools/chocolateyInstall.ps1
@@ -0,0 +1,41 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/Marcus-L/FreeVD/releases/download/v1.1.0.0/FreeVD.Setup.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'FreeVD*'
+
+  checksum      = '16F13A179BC14D812B7726DC263FD449E53E2487D67A656EA4D242374D118412'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
